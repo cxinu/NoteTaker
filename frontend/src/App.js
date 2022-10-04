be1b1d487b274b6314da5ad11f83ac9fded6ca94
cxinu <cxinu3099@gmail.com>
2022-10-05 04:13:15 +0530

    fucked up tailwind setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// import "./App.css";
+import "./App.css";
 import React from "react";
 import Header from "./components/Header.js";
 import NotesListPage from "./pages/NotesListPage";
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-primary w-full overflow-hidden">
       <Header />
       <BrowserRouter>
         <Routes>
