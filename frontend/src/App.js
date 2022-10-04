0c6d08f777d4aa214b41b5874d49bb90fa68bf31
cxinu <cxinu3099@gmail.com>
2022-10-05 01:31:31 +0530

    frontend dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import React from "react";
+import Header from "./components/Header.js";
+import NotesListPage from "./pages/NotesListPage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <p>Frontend</p>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NotesListPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
