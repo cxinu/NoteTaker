4b29c0d7770be02cf2db5c0be1769b0cba893762
cxinu <cxinu3099@gmail.com>
2022-10-06 15:18:10 +0530

    removed base app
    --unrequired

--- NoteTaker/settings.py
+++ NoteTaker/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base.apps.BaseConfig',
     'api.apps.ApiConfig',
     'rest_framework',
     'corsheaders',
